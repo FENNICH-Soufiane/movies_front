f329cf2075edd747f84923ecf4cd088049c9c2bb
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-24 22:02:47 +0100

    Different Color for Light & Dark Mode

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -13,19 +13,18 @@ export default function Signin() {
   // theme.method();
   
   return (
-    <div className="fixed inset-0 dark:bg-primary -z-10 flex justify-center items-center">
+    <div className="fixed inset-0 dark:bg-primary bg-white -z-10 flex justify-center items-center">
       <Container>
-      <form className="dark:bg-secondary rounded p-6 w-72 space-y-6">
+        <form className="dark:bg-secondary bg-white drop-shadow-lg rounded p-6 w-72 space-y-6">
           <Title>Sign in</Title>
           <FormInput label="Email" placeholder="john@email.com" name="email" />
           <FormInput label="Password" placeholder="********" name="password" />
-          <Submit />
+          <Submit value="Sign in" />
 
           <div className="flex justify-between">
-          <CustomLink to="/auth/forget-password">Forget password</CustomLink>
-          <CustomLink to="/auth/signup">Sign up</CustomLink>
+            <CustomLink to="/auth/forget-password">Forget password</CustomLink>
+            <CustomLink to="/auth/signup">Sign up</CustomLink>
           </div>
-          
         </form>
       </Container>
     </div>
